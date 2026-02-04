ae2e71a96621ceb28b1df9bb4c110fe05be754aa
Sasdarl <77890964+Sasdarl@users.noreply.github.com>
2026-02-03 20:24:34 -0500

    Add Huffman compression functionality
    It turned out this wasn't any form of LZ after all

--- xxxTRACT.py
+++ xxxTRACT.py
@@ -41,7 +41,7 @@ def xor(key, input_buffer): # Standard XOR encryption/decryption
     return output_buffer
 
 
-def de_lzw(input_buffer):
+def de_huff(input_buffer):
     if (ru32(input_buffer, 0) == 0):
         return -1 # This should not happen
 
@@ -51,30 +51,31 @@ def de_lzw(input_buffer):
     input_pos = 4
     dict_pos = 0
     output_pos = 0
+    
     dictionary = bytearray(0x200)
     
     while (input_pos < len(input_buffer) and input_pos < file_length):
         for i in range(0x100):
-            dictionary[i] = i # Load the dictionary with all possible values
-
-        dict_pos = 0
-        while (dict_pos <= 0xFF): # Phase 1: input values to dictionary
+            dictionary[i*2] = i # Load the dictionary with all possible values
+        dict_pos = 0 # This keeps the second bytes of each value intact, which seems like a bug but we have to keep it in
+        
+        while (dict_pos < 0x100): # Phase 1: input values to dictionary
             value_count = input_buffer[input_pos]
             input_pos += 1
-            if (value_count >= 0x80): # Use as offset for current position
+            if (value_count > 0x7F): # Use as offset for current position
                 dict_pos += value_count-0x7F
-                if (dict_pos <= 0xFF):
+                if (dict_pos < 0x100):
                     value_count = 0
-            if (dict_pos <= 0xFF):
-                dict_off = 0 # Begin setting dictionary values
-                while (dict_off <= value_count):
-                    dictionary[dict_pos] = input_buffer[input_pos]
+            if (dict_pos >= 0x100):
+                break
+            
+            for j in range(value_count+1):
+                dictionary[dict_pos*2] = input_buffer[input_pos]
+                input_pos += 1
+                if (dict_pos != dictionary[dict_pos*2]):
+                    dictionary[dict_pos*2+1] = input_buffer[input_pos]
                     input_pos += 1
-                    if (dict_pos != dictionary[dict_pos]):
-                        dictionary[dict_pos+0x100] = input_buffer[input_pos] # Yes, we have a second dictionary
-                        input_pos += 1 # Presumably for 16-bit values
-                    dict_pos += 1
-                    dict_off += 1
+                dict_pos += 1
         
         value_count = ru16(input_buffer, input_pos)
         input_pos += 2 # Phase 2: write to output
@@ -86,12 +87,109 @@ def de_lzw(input_buffer):
                 value_count -= 1
             else:
                 cur_input = sym_stack.pop()
-            if (cur_input == dictionary[cur_input]): # If we don't have a corresponding symbol, copy the byte
+            if (cur_input == dictionary[cur_input*2]): # If we don't have a corresponding symbol, copy the byte
                 output_buffer.append(cur_input)
             else:
-                sym_stack.append(dictionary[cur_input+0x100])
-                sym_stack.append(dictionary[cur_input])
+                sym_stack.append(dictionary[cur_input*2+1])
+                sym_stack.append(dictionary[cur_input*2])
                 
+    return output_buffer
+
+
+def re_huff(input_buffer): # We got there in the end
+    input_pos = 0
+    output_buffer = bytearray()
+    dictionary = bytearray(0x200)
+    sym_stack = bytearray()
+    min_sym_use = 3 # This seems to be the most efficient value
+    
+    while (input_pos < len(input_buffer)):
+        print(f"{len(output_buffer):X}")
+        passes = 2 # Worst-case, the second pass does nothing
+        
+        for i in range(0x100):
+            dictionary[i*2] = i # Load the dictionary with all possible values
+        
+        group_buffer = input_buffer[input_pos::] # Store a temporary buffer for the input we cover with the current dict
+        group_pos = 0
+        while (passes > 0):
+            use_byte_count = 0
+            use_byte_table = bytearray(0x200)
+            use_sym_table = bytearray()
+            
+            while (group_pos < len(group_buffer) and use_byte_count + len(use_sym_table) < 0x100): # Phase 1: assess symbol usage
+                cur_byte = group_buffer[group_pos]
+                
+                if (use_byte_table[cur_byte] == 0):
+                    use_byte_count += 1
+                if (use_byte_table[cur_byte] < 0xFE):
+                    use_byte_table[cur_byte] += 1 # Reserving 0xFF for processed symbols
+                if (group_pos > 0 and group_pos < len(group_buffer)-1): # Do not read from beyond the end of the buffer
+                    cur_short = ru16(group_buffer, group_pos-1)
+                    short_pos = 0
+                    while((short_pos%2 > 0 and short_pos > 0) or short_pos == 0): # Gather 16-bit symbols
+                        short_pos = sym_stack.find(wu16(cur_short), short_pos+1)
+                    if (short_pos >= 0 and use_sym_table[short_pos>>1] < 0xFE): # Increment 16-bit sym use
+                        use_sym_table[short_pos>>1] += 1
+                    else:
+                        sym_stack += wu16(cur_short)
+                        use_sym_table.append(1)
+                group_pos += 1
+            
+            group_buffer = group_buffer[0:group_pos]
+        
+            while (len(sym_stack) > 0): # Phase 2: input dictionary values
+                cur_sym = ru16(sym_stack, 0)
+                cur_sym_use = use_sym_table[0]
+                
+                if (cur_sym_use >= min_sym_use): # Symbols that fall below our threshold will be kept as-is
+                    dict_rep = -1
+                    while (dict_rep < 0 or dict_rep == cur_sym&0xFF):
+                        dict_rep = use_byte_table.find(0, dict_rep+1) # Only replace unused bytes!
+                    if (dict_rep >= 0):
+                        use_byte_table[dict_rep] = 0xFF # Mark this symbol as used (very important)
+                        dictionary[dict_rep*2:dict_rep*2+2] = wu16(cur_sym)
+                        group_buffer = group_buffer.replace(wu16(cur_sym),wu08(dict_rep)) # We can replace directly in our buffer
+                sym_stack = sym_stack[2::]
+                use_sym_table = use_sym_table[1::]
+            passes -= 1
+        input_pos += group_pos
+
+        dict_pos = 0
+        last_dict_pos = 0
+        while (dict_pos < 0x100): # Phase 3: build dictionary header
+            value_count = 0
+            if (dict_pos-last_dict_pos >= 0x7F): # Since offsets start at 0x80, they can't go too far
+                output_buffer.append(0x7F+0x7F)
+                last_dict_pos += 0x7F
+                value_count = 1
+            elif (ru16(dictionary,dict_pos*2) != dict_pos):
+                if (dict_pos-last_dict_pos > 0):
+                    output_buffer.append(dict_pos-last_dict_pos+0x7F) # As long as our pos is an 8-bit value, we have to put in one symbol
+                    last_dict_pos = dict_pos
+                    value_count = 1
+                else:
+                    while (dict_pos+value_count < 0xFF and value_count < 0x7F and ru16(dictionary, (dict_pos+value_count)*2) != dict_pos+value_count):
+                        value_count += 1 # Count up the values we have to replace
+                    if (value_count > 0):
+                        output_buffer.append(value_count-1) # Only add the count if we're actually going to use it!
+            if (value_count == 0): # If we didn't get anything, keep going through the dictionary
+                dict_pos += 1
+            if (dict_pos >= 0x100): # If we're out of 8-bit range, we're DONE with the dictionary header
+                break
+            for i in range(value_count):
+                if (dict_pos < 0xFF and dictionary[dict_pos*2] != dict_pos): # Don't replace symbols we're using for other symbols
+                    output_buffer += dictionary[dict_pos*2:dict_pos*2+2]
+                else:
+                    output_buffer.append(dict_pos)
+                dict_pos += 1
+                last_dict_pos = dict_pos
+        if (dict_pos > last_dict_pos): # Ensure the header reaches 0x100 or higher so it can end
+            output_buffer.append(dict_pos-last_dict_pos+0x7F)
+        output_buffer += wu16(len(group_buffer)) # Prepend size of data section
+        output_buffer += group_buffer
+    
+    output_buffer = wu32(len(output_buffer)) + output_buffer
     return output_buffer
 
 
@@ -115,10 +213,10 @@ def de_rle(input_buffer):
         if (curbyte == 0 and input_pos < len(input_buffer)-1):
             curshort = ru16(input_buffer, input_pos) # Load symbol
             input_pos += 2
-            off = curshort//0x10 # The last 12 bytes are symbol offset
+            off = curshort//0x10 # The last 12 bits are symbol offset
             if off == 0: # I can only assume this is possible
                 return output_buffer
-            length = (curshort&0x0F) # The first four bytes are symbol length
+            length = (curshort&0x0F) # The first four bits are symbol length
             
             for i in range(length+2):
                 newbyte = sym_buffer[(off-1+i)%0x1000] # Copy bytes
@@ -134,7 +232,6 @@ def de_rle(input_buffer):
         if (head_count >= 8): # Increment header position once we're done with a byte
             head_pos += 1
             head_count = 0
-    
     return output_buffer
 
 
@@ -270,9 +367,9 @@ def repack_file(input_folder, output_folder, output_file, recurse, write=True):
             file_props[i][0] = len(output_buffer)
             file_props[i][1] = len(input_buffer)
                         
+            print(filenames[i])
             if file_props[i][2]%0x10 == 2:
-                #re_lzw(input_buffer) # I'm. NOT. doing this.
-                file_props[i][2] -= 2 # I'd love to compress with RLE instead, but the game hates it
+                input_buffer = re_huff(input_buffer) # We do this now.
             if file_props[i][2]%0x10 == 1:
                 input_buffer = re_rle(input_buffer)
             input_buffer = xor(key, input_buffer)
@@ -322,20 +419,19 @@ def unpack_file(input_buffer, output_folder, output_name, recurse):
     output_folder = output_folder+"/"+output_name+"/"
     key = input_buffer[0x08:0x08+ru32(input_buffer,0x04)]
     file_pos = 0x08+len(key) # Set up XOR decryption
-    section_size = []
     filenames = []
     file_props = []
+
+    index_pos = ru32(input_buffer,file_pos)
+    index_size = ru32(input_buffer,file_pos+0x04)
+    data_pos = ru32(input_buffer,file_pos+0x08)
+    data_size = ru32(input_buffer,file_pos+0x0C)
     
-    while (file_pos < len(input_buffer)-3):
-        section_size.append(ru32(input_buffer, file_pos))
-        file_pos += 0x04 # As far as I can tell, only the first two sections are important
-    file_pos = section_size[0]
-    
-    listfile = input_buffer[file_pos:file_pos+section_size[1]]
+    listfile = input_buffer[index_pos:index_pos+index_size]
     if (len(key) > 0): # Get the list of filenames and file properties
         listfile = xor(key, listfile)
     
-    file_pos += section_size[1]
+    file_pos += data_size
     for j in range(0, len(listfile), 0x40): # Serialize our file properties
         filenames.append(listfile[j:j+0x20].decode("utf-8").rstrip("\x00"))
         file_props.append([ru32(listfile, j+0x30),
@@ -346,7 +442,7 @@ def unpack_file(input_buffer, output_folder, output_name, recurse):
         output.write(listfile) # Write the list file to its own file
         output.close()
     
-    compfile = input_buffer[file_pos:len(input_buffer)] # We're done with the header now
+    compfile = input_buffer[data_pos:data_pos+data_size] # We're done with the header now
     for i in range(0, len(filenames)):
         curfile = compfile[file_props[i][0]:file_props[i][0]+file_props[i][1]] # Get current file
         
@@ -360,7 +456,7 @@ def unpack_file(input_buffer, output_folder, output_name, recurse):
             curfile = xor(key, curfile)
 
         if (file_props[i][2] % 0x10 == 2): # Use properties to select decompression algorithm
-            decomp_file = de_lzw(curfile)
+            decomp_file = de_huff(curfile)
         elif (file_props[i][2] % 0x10 == 1):
             decomp_file = de_rle(curfile)
         else:
@@ -371,6 +467,9 @@ def unpack_file(input_buffer, output_folder, output_name, recurse):
             with open(filename, "wb") as output:
                 output.write(decomp_file)
                 output.close()
+            with open(filename+".TEMP", "wb") as tempput:
+                tempput.write(curfile)
+                tempput.close()
             
             if (recurse and Path(filename).suffix == ".PAK"): # And now, unpacking within unpacking awaits you
                 unpack_file(decomp_file, f"{Path(filename).parent}", Path(filename).stem+"_decompressed", recurse)
